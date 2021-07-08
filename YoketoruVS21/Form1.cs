66b3e1521c17f0a1b84aad41678adc7b4cd158b6
A601-10 <tnk.dat.class@gmail.com>
2021-07-08 10:04:17 +0900

    当たり判定と跳ね返り

--- YoketoruVS21/Form1.cs
+++ YoketoruVS21/Form1.cs
@@ -16,12 +16,16 @@ namespace YoketoruVS21
     {
         const bool isDebug = true;
 
+        const int SpeedMax = 20;
+
         const int PlayerMax = 1;
         const int EnemyMax = 3;
         const int ItemMax = 3;
         const int ChrMax = PlayerMax + EnemyMax + ItemMax;
 
         Label[] chrs = new Label[ChrMax];
+        int[] vx = new int[ChrMax];
+        int[] vy = new int[ChrMax];
 
         const int PlayerIndex = 0;
         const int EnemyIndex = PlayerMax;
@@ -132,6 +136,8 @@ namespace YoketoruVS21
                     {
                         chrs[i].Left = rand.Next(ClientSize.Width - chrs[i].Width);
                         chrs[i].Top = rand.Next(ClientSize.Height - chrs[i].Height);
+                        vx[i] = rand.Next(-SpeedMax, SpeedMax + 1);
+                        vy[i] = rand.Next(-SpeedMax, SpeedMax + 1);
                     }
                     break;
 
@@ -154,6 +160,42 @@ namespace YoketoruVS21
             Point mp = PointToClient(MousePosition);
 
             //TODO:mpがプレイヤーのラベルの中心になるように設定
+
+            chrs[PlayerIndex].Left = mp.X - chrs[PlayerIndex].Width / 2;
+            chrs[PlayerIndex].Top = mp.Y - chrs[PlayerIndex].Height / 2;
+
+            for(int i=EnemyIndex;i<ChrMax; i++)
+            {
+                chrs[i].Left += vx[i];
+                chrs[i].Top += vy[i];
+
+                if(chrs[i].Left<0)
+                {
+                    vx[i] = Math.Abs(vx[i]);
+                }
+                if (chrs[i].Top < 0)
+                {
+                    vy[i] = Math.Abs(vy[i]);
+                }
+                if (chrs[i].Right > ClientSize.Width)
+                {
+                    vx[i] = -Math.Abs(vx[i]);
+                }
+                if (chrs[i].Bottom > ClientSize.Height)
+                {
+                    vy[i] = -Math.Abs(vy[i]);
+                }
+
+                //当たり判定
+                if((mp.X>=chrs[i].Left)
+                    &&(mp.X<chrs[i].Right)
+                    &&(mp.Y>=chrs[i].Top)
+                    &&(mp.Y<chrs[i].Bottom))
+                    {
+                    MessageBox.Show("重なった!!");
+                }
+            }
+
         }
 
         private void titleButton_Click(object sender, EventArgs e)
